c2440565a6e6448e7f947636ecf9ead5f1359ac3
Miguel C <immac.gm@gmail.com>
2013-02-12 14:43:48 -0600

    Inicial
    Pruebas iniciales

---
+++ src/Logic/Player.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+/**
+ *
+ * @author Immac
+ */
+public class Player {
+    
+}
